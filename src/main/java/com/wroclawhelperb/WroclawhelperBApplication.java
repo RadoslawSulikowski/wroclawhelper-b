8c2c452f041d2e7f76ba7d43ed801d40f767de05
RadoslawSulikowski <radoslaw.sulikowski@gmail.com>
2020-01-12 20:45:58 +0100

    1

---
+++ src/main/java/com/wroclawhelperb/WroclawhelperBApplication.java
@@ -0,0 +1,13 @@
+package com.wroclawhelperb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WroclawhelperBApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WroclawhelperBApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wroclawhelperb/WroclawhelperBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wroclawhelperb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WroclawhelperBApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
