43f3207e6160376db66551e6da460adec3af7d84
RadoslawSulikowski <radoslaw.sulikowski@gmail.com>
2020-02-02 19:37:50 +0100

    MailBuilder added

--- src/main/java/com/wroclawhelperb/service/mail/Mail.java
+++ src/main/java/com/wroclawhelperb/service/mail/Mail.java
@@ -1,15 +1,45 @@
 package com.wroclawhelperb.service.mail;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
-@AllArgsConstructor
-@NoArgsConstructor
+import java.util.ArrayList;
+import java.util.List;
+
 @Getter
-public class Mail {
-    private String sendTo;
-    private String subject;
-    private String message;
+public final class Mail {
+    private final String sendTo;
+    private final String subject;
+    private final String message;
 
+    public static class MailBuilder {
+        private String sendTo;
+        private String subject;
+        private List<String> messageLines = new ArrayList<>();
+
+        public MailBuilder sendTo(String sendTo) {
+            this.sendTo = sendTo;
+            return this;
+        }
+
+        public MailBuilder subject(String subject) {
+            this.subject = subject;
+            return this;
+        }
+
+        public MailBuilder messegeLine(String line) {
+            messageLines.add(line);
+            return this;
+        }
+
+        public Mail build() {
+            String message = String.join("\n", messageLines);
+            return new Mail(sendTo, subject, message);
+        }
+    }
+
+    private Mail(final String sendTo, final String subject, String message) {
+        this.sendTo = sendTo;
+        this.subject = subject;
+        this.message = message;
+    }
 }
