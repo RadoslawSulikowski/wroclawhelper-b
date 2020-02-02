5fd8f9771caf19768b1467b5f61e9510f8e02101
RadoslawSulikowski <radoslaw.sulikowski@gmail.com>
2020-02-02 21:45:05 +0100

    Put endpoint updateStation adde

--- src/main/java/com/wroclawhelperb/controller/WeatherStationController.java
+++ src/main/java/com/wroclawhelperb/controller/WeatherStationController.java
@@ -1,6 +1,8 @@
 package com.wroclawhelperb.controller;
 
 import com.wroclawhelperb.domain.weather.WeatherStationDto;
+import com.wroclawhelperb.exception.BikeStationNotFoundException;
+import com.wroclawhelperb.exception.WeatherStationNotFoundException;
 import com.wroclawhelperb.service.WeatherStationService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -26,5 +28,10 @@ public class WeatherStationController {
     @ResponseStatus(value = HttpStatus.CREATED, reason = "Weather station successful added")
     public void addNewWeatherStation(@RequestBody WeatherStationDto station) {
         weatherStationService.addNewWeatherStation(station);
+    }
+
+    @PutMapping
+    public WeatherStationDto updateStation(@RequestBody WeatherStationDto station) throws WeatherStationNotFoundException {
+        return weatherStationService.updateStation(station);
     }
 }
--- src/main/java/com/wroclawhelperb/service/WeatherStationService.java
+++ src/main/java/com/wroclawhelperb/service/WeatherStationService.java
@@ -1,6 +1,9 @@
 package com.wroclawhelperb.service;
 
+import com.wroclawhelperb.domain.weather.WeatherStation;
 import com.wroclawhelperb.domain.weather.WeatherStationDto;
+import com.wroclawhelperb.exception.BikeStationNotFoundException;
+import com.wroclawhelperb.exception.WeatherStationNotFoundException;
 import com.wroclawhelperb.mapper.WeatherStationMapper;
 import com.wroclawhelperb.repository.WeatherStationRepository;
 import org.springframework.stereotype.Service;
@@ -32,5 +35,15 @@ public class WeatherStationService {
 
     public void addNewWeatherStation(WeatherStationDto station) {
         weatherStationRepository.save(weatherStationMapper.mapToWeatherStation(station));
+    }
+
+    public WeatherStationDto updateStation(WeatherStationDto station) throws WeatherStationNotFoundException {
+        WeatherStation weatherStation = weatherStationRepository.findById(station.getShortName())
+                .orElseThrow(WeatherStationNotFoundException::new);
+        weatherStation.setName(station.getName());
+        weatherStation.getLocation().setLatitude(station.getLocation().getLatitude());
+        weatherStation.getLocation().setLongitude(station.getLocation().getLongitude());
+        return weatherStationMapper.mapToWeatherStationDto(
+                weatherStationRepository.save(weatherStation));
     }
 }
